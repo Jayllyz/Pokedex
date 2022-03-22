76b25c977eca05ddcd2c85fbdba7ca79b1d35c32
Jayllyz <antonydavid945@gmail.com>
2022-03-22 13:45:41 +0100

    update forms

--- connexion.php
+++ connexion.php
@@ -10,18 +10,30 @@ include "includes/head.php";
     <h1><strong>Connexion</strong></h1>
     <div class="container">
         <div class="connexion">
-            <h3><strong>Je posède un compte</strong></h3>
-            <form action="" method="post">
-                <input type="email" name="email" placeholder="Email" required>
+            <h3><strong>Je possède un compte</strong></h3>
+            <form action="verifications/verif_connexion.php" method="post">
+                <input type="email" name="email" placeholder="Email" value="<?= isset(
+                  $_COOKIE["email"]
+                )
+                  ? $_COOKIE["email"]
+                  : "" ?>" required>
                 <input type="password" name="password" placeholder="Mot de passe" required>
                 <input type="submit" class="submit" value="Connexion">
             </form>
         </div>
         <div class="inscription">
         <h3><strong>Je crée un compte</strong></h3>
-            <form action="" method="post" enctype="multipart/form-data">
-                <input type="text" name="pseudo" placeholder="Pseudo" required>
-                <input type="email" name="email" placeholder="Email" required>
+            <form action="verifications/verif_inscription.php" method="post" enctype="multipart/form-data">
+                <input type="text" name="pseudo" placeholder="Pseudo" value="<?= isset(
+                  $_COOKIE["pseudo"]
+                )
+                  ? $_COOKIE["pseudo"]
+                  : "" ?>" required> 
+                <input type="email" name="email" placeholder="Email" value="<?= isset(
+                  $_COOKIE["email"]
+                )
+                  ? $_COOKIE["email"]
+                  : "" ?>" required> 
                 <input type="password" name="password" placeholder="Mot de passe" required>
                 <label><strong>Image de profil: </strong>
                 <input type="file" name="image" class="image" accept="image/png, image/jpeg"></label>
