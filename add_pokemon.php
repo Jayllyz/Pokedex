79de6e4070598bf09416a75fa9891e6b149c2e4e
Danette10 <dan.sebag1007@gmail.com>
2022-03-23 12:39:05 +0100

    head

---
+++ add_pokemon.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php
+$title = "Ajouter un pokémon";
+$linkCss = "css/style.css";
+$linkLogoOnglet = "images/pikachu.png";
+include "includes/head.php";
+?>
+<body>
+<?php include "includes/header.php"; ?>
+
+
+
+
+<?php include "includes/footer.php"; ?>
+</body>
+</html>
--- collection.php
+++ collection.php
@@ -1,1 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php
+$title = "Collection";
+$linkCss = "css/style.css";
+$linkLogoOnglet = "images/pikachu.png";
+include "includes/head.php";
+?>
+<body>
+    <?php include "includes/header.php"; ?>
 
+
+
+
+    <?php include "includes/footer.php"; ?>
+</body>
+</html>
